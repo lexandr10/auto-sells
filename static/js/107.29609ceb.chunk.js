e2d187fb72d962dd50a120011d28a3533ece79aa
lexandr10 <133785276+lexandr10@users.noreply.github.com>
2024-05-20 18:11:41 +0000

    Deploying to gh-pages from @ lexandr10/auto-sells@421906480e96b3850a82e02c60f7e3b22be07c8f 🚀

--- static/js/107.29609ceb.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkauto_sells=self.webpackChunkauto_sells||[]).push([[107],{1770:function(e,i,n){n.r(i);var r=n(6317),a=n(4420),t=n(6676),l=n(184);i.default=function(){var e=(0,a.v9)(t.gZ);return(0,l.jsx)("ul",{children:e.length>0&&e.map((function(e){return(0,l.jsx)(r.Z,{car:e,gallery:e.gallery,details:e.details,reviews:e.reviews,price:e.price,rating:e.rating,location:e.location,description:e.description,id:e._id,name:e.name})}))})}}}]);
-//# sourceMappingURL=107.29609ceb.chunk.js.map
